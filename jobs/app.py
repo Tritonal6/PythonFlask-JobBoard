cdd3d5f94bda7d4650471d2eac5554935c5e8a6f
tritonal6 <ianbrenner6@gmail.com>
2019-04-03 23:07:36 +0000

    Setup on ChromeOS, setup initial database connections

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 
 app = Flask(__name__)
 
+PATH = 'db/jobs.sqlite'
+# Defines the path within the app to the database.
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# Create a function to easily query the db
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+#We want to ensure db is closed when app instance is shut down:
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route('/')
 @app.route('/jobs')
